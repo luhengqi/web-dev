5aef96857eaf6bbad487475bcb359e004ca694a2
Luhengqi <luhengqi@gmail.com>
2018-08-06 14:46:30 +0800

    .typo

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,21 +1,6 @@
 /*
-* @Author: Luhengqi
-* @Date:   2017-11-17 14:23:27
-* @Last Modified by:   Luhengqi
-* @Last Modified time: 2018-08-02 13:15:28
-*/
-
-$(document).ready(function() {
-	var $body = $('body');
-
-	function abc() {
-		$body.css('background', '#000');
-	}
-
-	function bbc() {
-		$body.css('color', '#000');
-	}
-
-	abc();
-	bbc();
-});
+ * @Author: Luhengqi
+ * @Date:   2017-11-17 14:23:27
+ * @Last Modified by:   Luhengqi
+ * @Last Modified time: 2018-08-06 14:44:41
+ */
